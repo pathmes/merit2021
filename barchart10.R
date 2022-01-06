64e223f5fac34a2ab501d092db56637618345cca
Pathmeswaran <pathmes@kln.ac.lk>
2022-01-06 23:33:34 +0530

    Add comments

--- barchart10.R
+++ barchart10.R
@@ -1,29 +1,113 @@
+# Load required packages
 library(tidyverse)
 library(readr)
-df1 <- read_csv("merit list 2021.csv", col_types = "ci")
+library(colorspace)
+
+# Import data
+df1 <- read_csv("merit list 2021.csv", col_types = "fi")
 
 
+# Create 10 ordered groups (Tenths) based on 'Merit_order'
 df1 <-  df1 %>% 
-  mutate(Tenths = ntile(Merit_order,10))
-df1$Tenths <- as.integer(df1$Tenths)
+  mutate(Tenths = desc(ntile(Merit_order,10)))
+# df1$Tenths <- as.integer(df1$Tenths)
 
+# Create the variable - 'utotal' - indicating the total number of students 
+# per university
 df1 <-  df1 %>% 
   group_by(University) %>% 
   mutate(utotal = n())
 
+# Create the variable - 'utenths' indicating the number of students in
+# each category of Tenths per university
 df1 <-  df1 %>% 
   group_by(University, Tenths) %>% 
   mutate(utenths = n())
 
+# Create the variable - 'utenths_p' indicating the percentage of students 
+# within each university in each category of Tenths
 df1 <-  df1 %>% 
   group_by(University, Tenths) %>% 
   mutate(utenths_p = utenths*100/utotal)
 
+
+
+
+# Keep just one record for each unique combination of university & tenths
 df10 <- df1 %>% 
   distinct(University, Tenths, .keep_all = TRUE)
 
-ggplot(data = df10, mapping = aes(x = University, y = utenths_p, fill = Tenths)) + 
+
+
+
+p1 <- ggplot(data = df10, mapping = 
+               aes(x = University, y = utenths_p, 
+                   fill = Tenths)) + 
   geom_bar(stat = "identity") +
-  scale_y_continuous(breaks = seq(0, 100, 20)) +
-  labs(y = "Percent of Students")
+  scale_y_continuous(breaks = seq(0, 100, 10)) +
+  labs(y = "Cumulative percent of Students")
+
+p1 + scale_fill_continuous_divergingx(palette = "Geyser")
+p1 + scale_fill_gradient2(low="red", mid="yellow", high="green", 
+                          midpoint = -5)
+
+# The default ordering of the universities is distracting.  
+# Better to order the change the order based on some information in the merit list.
+# Order the universties according to the merit position of the middle student of
+# each university
+
+# Identify the merit position of the middle student of each university - 'medrank'
+df1 <-  df1 %>% 
+  group_by(University) %>% 
+  mutate(medrank = median(Merit_order))
+
+# Create a data frame of University ranked by median merit position 'mr'
+df8 <- df1 %>% ungroup() %>%
+  distinct(University,  .keep_all = TRUE)
+df8$mr <- rank(df8$medrank)
+df8 <- df8 %>% select(University, mr)
+
+# Add the variable 'mr' to the dataframe used to make the graphs
+df10 <- inner_join(df8, df10, by = "University")
+
+
+p2 <- ggplot(data = df10, mapping = 
+               aes(x = fct_reorder(University, mr), y = utenths_p, 
+                   fill = Tenths)) + 
+  geom_bar(stat = "identity") +
+  scale_y_continuous(breaks = seq(0, 100, 10)) +
+  labs(y = "Cumulative percent of Students",
+       x = "University",
+       fill = "Merit Order")  
+  
+
+p21 <- p2 + scale_fill_gradient2(low="brown", mid="yellow", high="green",
+midpoint = -5) +  theme(legend.position = "none") 
+
+# Not a pleasing color scheme - try 'Geyser'
+# Increase the visibility of the grid lines by making the background darker 
+p2 + scale_fill_continuous_divergingx(palette = "Geyser", 
+                                      mid = -5, rev = TRUE) +
+  theme(panel.background = element_rect(fill = "gray86"))
+
+p2 + scale_fill_continuous_divergingx(palette = "Geyser",
+mid = -5, rev = TRUE) +
+theme(panel.background = element_rect(fill = "gray70"), legend.position = "none")
+
+ggplot(data = df10, mapping =
+         aes(x = fct_reorder(University, mr), y = utenths_p,
+             fill = Tenths)) +
+  geom_bar(stat = "identity") +
+  scale_y_continuous(breaks = seq(0, 100, 10)) +
+  labs(y = "Cumulative Percent of Students",
+       x = "University",
+       title = "Common Merit List Positions by University - 2021",
+       caption = "Data from the UGC",
+       fill = "Merit Order")  +
+  scale_fill_continuous_divergingx(palette = "Geyser",
+                                   mid = -5, rev = TRUE,
+                                   breaks=c(-1.5,-5, -9.5),
+                                   labels=c("Top","Middle","Bottom")) + 
+  theme(panel.background = element_rect(fill = "gray70"))
+
 
